a329c3a31b88966eacf9f7dc02805c3d13deda99
xueweiyema <xueweiyema@hotmail.com>
2018-09-16 22:38:19 +0800

    complete 1st

--- models.py
+++ models.py
@@ -26,18 +26,22 @@ class Net(nn.Module):
 
         self.pool1 = nn.MaxPool2d(2, 2)
         self.bn1 = nn.BatchNorm2d(32)
+        self.drop1 = nn.Dropout(p=0.5)
 
         self.conv2 = nn.Conv2d(32, 64, 5)
         self.pool2 = nn.MaxPool2d(2, 2)
         self.bn2 = nn.BatchNorm2d(64)
+        self.drop2 = nn.Dropout(p=0.5)
 
         self.conv3 = nn.Conv2d(64, 128, 5)
         self.pool3 = nn.MaxPool2d(2, 2)
         self.bn3 = nn.BatchNorm2d(128)
+        self.drop3 = nn.Dropout(p=0.5)
 
         self.conv4 = nn.Conv2d(128, 256, 5)
         self.pool4 = nn.MaxPool2d(2, 2)
         self.bn4 = nn.BatchNorm2d(256)
+        self.drop4 = nn.Dropout(p=0.5)
 
         self.fc1 = nn.Linear(25600, 1024)
         self.fc2 = nn.Linear(1024, 512)
